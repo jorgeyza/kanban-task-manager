fba46111c7229b8f3debb80814ee4b99af182997
Jorge <jorgeyza@gmail.com>
2022-11-28 21:12:24 -0500

    Fix PORT env validation

--- src/env/schema.mjs
+++ src/env/schema.mjs
@@ -7,7 +7,7 @@ import { z } from 'zod';
  */
 export const serverSchema = z.object({
   DATABASE_URL: z.string().url(),
-  PORT: z.number().int(),
+  PORT: z.string(),
   NODE_ENV: z.enum(['development', 'test', 'production'])
 });
 
