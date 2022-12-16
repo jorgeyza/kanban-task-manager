13ad17f12986382fdebaf5ee2a541679f946c93e
Jorge <jorgeyza@gmail.com>
2022-12-15 23:11:50 -0500

    Add assets

---
+++ src/assets/index.ts
@@ -0,0 +1,33 @@
+import addTaskMobileIcon from './icon-add-task-mobile.png';
+import BoardIconSVG from './icon-board';
+import checkIcon from './icon-check.png';
+import chevronDownIcon from './icon-chevron-down.png';
+import chevronUpIcon from './icon-chevron-up.png';
+import crossIcon from './icon-cross.png';
+import HideSidebarIconSVG from './icon-hide-sidebar';
+import ShowSidebarIconSVG from './icon-show-sidebar';
+import verticalEllipsisIcon from './icon-vertical-ellipsis.png';
+import Logo from './logo';
+import logoMobile from './logo-mobile.png';
+import purpleCheckIcon from './purple-check-icon.png';
+import LightThemeIconSVG from './icon-light-theme';
+import DarkThemeIconSVG from './icon-dark-theme';
+
+// SVGs minified with SVGOMG https://jakearchibald.github.io/svgomg/
+
+export {
+  addTaskMobileIcon,
+  BoardIconSVG,
+  checkIcon,
+  chevronDownIcon,
+  chevronUpIcon,
+  crossIcon,
+  HideSidebarIconSVG,
+  ShowSidebarIconSVG,
+  verticalEllipsisIcon,
+  Logo,
+  logoMobile,
+  purpleCheckIcon,
+  LightThemeIconSVG,
+  DarkThemeIconSVG
+};
