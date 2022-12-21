d43f40f66174ca589f5924cbfc239659216a019d
Jorge Eyzaguirre <jorgeyza@gmail.com>
2022-12-21 13:55:43 -0500

    Add Board

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,17 +3,19 @@ import Head from 'next/head';
 
 import { trpc } from '@/utils/trpc';
 
+import Board from '@/components/Board';
+
 const Home: NextPage = () => {
   const hello = trpc.example.hello.useQuery({ text: 'from tRPC' });
 
   return (
     <>
       <Head>
-        <title>Kanban Manager</title>
+        <title>Kanban task manager</title>
         <meta name='description' content='Kanban task manager' />
         <link rel='icon' href='/favicon.ico' />
       </Head>
-      <div>Here goes a project</div>
+      <Board />
     </>
   );
 };
