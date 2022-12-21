b6674d38aab78cda120a83b539dbc03aea609e72
Jorge Eyzaguirre <jorgeyza@gmail.com>
2022-12-21 13:51:45 -0500

    Update Header styles

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,31 +1,31 @@
-import { Box, Button, Flex, Heading, useColorModeValue } from '@chakra-ui/react';
-import Image from 'next/image';
+import { Button, Flex, Heading, useColorModeValue } from '@chakra-ui/react';
 
-import { verticalEllipsisIcon } from '@/assets';
+import { VerticalEllipsisIconSVG } from '@/assets';
 
 const Header = () => {
   const borderColor = useColorModeValue('lightGray', 'lightGrayAlpha25');
   const backgroundColor = useColorModeValue('white', 'darkerGray');
+  const headingColor = useColorModeValue('black', 'white');
+
   return (
     <Flex
       as='header'
       backgroundColor={backgroundColor}
       justifyContent='space-between'
-      height='96px'
+      height='97px'
       alignItems='center'
       padding={5}
       borderBottomColor={borderColor}
-      borderBottomWidth='thin'>
-      <Heading as='h2' size='md'>
+      borderBottomWidth='thin'
+    >
+      <Heading as='h1' size='md' color={headingColor}>
         Platform Launch
       </Heading>
       <Flex alignItems='center' columnGap={6}>
-        <Button variant='primary' size='lg'>
+        <Button variant='primary' backgroundColor='customPurple.500' size='lg'>
           + Add New Task
         </Button>
-        <Box position='relative'>
-          <Image src={verticalEllipsisIcon} alt='options menu' />
-        </Box>
+        <VerticalEllipsisIconSVG />
       </Flex>
     </Flex>
   );
