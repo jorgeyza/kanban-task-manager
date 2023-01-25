5aebaf9107ec67e2075c5c3ff9e9b11e1c115ee7
Jorge Eyzaguirre <jorgeyza@gmail.com>
2023-01-25 12:34:00 -0500

    Add comment

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,39 +1,43 @@
-import { baseTheme as base, extendTheme, type ThemeConfig } from '@chakra-ui/react';
+import {
+  baseTheme as base,
+  extendTheme,
+  type ThemeConfig,
+} from "@chakra-ui/react";
 
-import { globalStyles as styles } from './styles';
-import components from './components';
+import { globalStyles as styles } from "./styles";
+import components from "./components";
 
 const config: ThemeConfig = {
-  initialColorMode: 'light',
-  useSystemColorMode: false
+  initialColorMode: "light",
+  useSystemColorMode: false,
 };
 
 const colors = {
-  black: '#000112',
-  customGray: '#828FA3',
-  white: '#FFFFFF',
-  whiteSoft: '#EFF6FF',
-  lightGray: '#E4EBFA',
-  lightGrayAlpha25: 'rgba(130, 143, 163, .25)',
-  lighterGray: '#F4F7FD',
-  darkGray: '#3E3F4E',
-  darkerGray: '#2B2C37',
+  black: "#000112",
+  customGray: "#828FA3",
+  white: "#FFFFFF",
+  whiteSoft: "#EFF6FF",
+  lightGray: "#E4EBFA",
+  lightGrayAlpha25: "rgba(130, 143, 163, 0.25)",
+  lighterGray: "#F4F7FD",
+  darkGray: "#3E3F4E",
+  darkerGray: "#2B2C37",
   customPurple: {
     ...base.colors.purple,
-    200: '#9895DA',
-    500: '#635FC7',
-    600: '#5D59C5'
+    200: "#9895DA",
+    500: "#635FC7",
+    600: "#5D59C5",
   },
-  purpleAlpha25: 'rgba(99, 95, 199, .25)',
-  lightPurple: '#A8A4FF',
-  customRed: '#EA5555',
-  lightRed: '#FF9898',
-  lightBlack: '#20212C'
+  purpleAlpha25: "rgba(99, 95, 199, .25)",
+  lightPurple: "#A8A4FF",
+  customRed: "#EA5555",
+  lightRed: "#FF9898",
+  lightBlack: "#20212C",
 };
 
 const fonts = {
   heading: `Plus Jakarta Sans, sans-serif`,
-  body: `Plus Jakarta Sans, sans-serif`
+  body: `Plus Jakarta Sans, sans-serif`,
 };
 
 const theme = extendTheme({
@@ -44,12 +48,33 @@ const theme = extendTheme({
   semanticTokens: {
     colors: {
       mainBackgroundColor: {
-        default: 'lighterGray',
-        _dark: 'lightBlack'
-      }
-    }
+        default: "lighterGray",
+        _dark: "lightBlack",
+      },
+    },
   },
-  styles
+  styles,
 });
 
 export default theme;
+
+// I had to use variants with many components instead of just using textStyles because of a bug https://github.com/chakra-ui/chakra-ui/issues/3884
+
+// const textStyles = {
+//   'modal-title': {
+//     fontSize: '18px',
+//     fontWeight: 'bold',
+//     color: 'black',
+//     _dark: {
+//       color: 'white'
+//     }
+//   },
+//   'modal-subtitle': {
+//     fontSize: '12px',
+//     fontWeight: 'bold',
+//     color: 'black',
+//     _dark: {
+//       color: 'white'
+//     }
+//   }
+// };
