b8d18ed48a79f4623c5eb5c324db4ada047e9edf
Jorge Eyzaguirre <jorgeyza@gmail.com>
2023-01-25 12:29:17 -0500

    Add AddNewTaskModal component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,221 @@
+import {
+  chakra,
+  Box,
+  Button,
+  Flex,
+  Heading,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalHeader,
+  ModalOverlay,
+  Select,
+  Textarea,
+  FormErrorMessage,
+  FormLabel,
+  FormControl,
+  Input,
+  useColorModeValue,
+  useDisclosure,
+} from "@chakra-ui/react";
+import { useForm } from "react-hook-form";
+
+import { VerticalEllipsisIconSVG } from "@/assets";
+
+const Header = () => {
+  const borderColor = useColorModeValue("lightGray", "lightGrayAlpha25");
+  const backgroundColor = useColorModeValue("white", "darkerGray");
+  const headingColor = useColorModeValue("black", "white");
+
+  const { isOpen, onOpen, onClose, getButtonProps, getDisclosureProps } =
+    useDisclosure();
+  const addNewTaskModalButtonProps = getButtonProps();
+  const addNewTaskModalDisclosureProps = getDisclosureProps();
+
+  const {
+    handleSubmit,
+    register,
+    formState: { errors, isSubmitting },
+  } = useForm();
+
+  function onSubmit(values) {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        alert(JSON.stringify(values, null, 2));
+        resolve();
+      }, 3000);
+    });
+  }
+
+  return (
+    <>
+      <Flex
+        as="header"
+        backgroundColor={backgroundColor}
+        justifyContent="space-between"
+        height="97px"
+        alignItems="center"
+        padding={5}
+        borderBottomColor={borderColor}
+        borderBottomWidth="thin"
+      >
+        <Heading as="h1" size="md" color={headingColor}>
+          Platform Launch
+        </Heading>
+        <Flex alignItems="center" columnGap={6}>
+          <Button
+            variant="primary"
+            size="lg"
+            backgroundColor="customPurple.500"
+            onClick={onOpen}
+            {...addNewTaskModalButtonProps}
+          >
+            + Add New Task
+          </Button>
+          <Menu>
+            <MenuButton as={Box} cursor="pointer">
+              <VerticalEllipsisIconSVG />
+            </MenuButton>
+            <MenuList>
+              <MenuItem>Edit Board</MenuItem>
+              <MenuItem color="customRed">Delete Board</MenuItem>
+            </MenuList>
+          </Menu>
+        </Flex>
+      </Flex>
+      <Modal
+        isOpen={isOpen}
+        onClose={onClose}
+        isCentered
+        size="lg"
+        {...addNewTaskModalDisclosureProps}
+      >
+        <ModalOverlay />
+        <ModalContent backgroundColor={backgroundColor} padding={8} rowGap={6}>
+          <ModalHeader padding={0}>
+            <Heading as="h4" variant="modal-title">
+              Add New Task
+            </Heading>
+          </ModalHeader>
+          <ModalBody
+            display="flex"
+            flexDirection="column"
+            rowGap={6}
+            padding={0}
+          >
+            <chakra.form
+              display="flex"
+              flexDirection="column"
+              rowGap={6}
+              onSubmit={handleSubmit(onSubmit)}
+            >
+              <FormControl
+                borderColor="lightGrayAlpha25"
+                isInvalid={errors.title}
+              >
+                <FormLabel htmlFor="title" variant="modal-subtitle">
+                  Title
+                </FormLabel>
+                <Input
+                  id="title"
+                  placeholder="e.g Take coffee break"
+                  {...register("title", {
+                    required: "This is required",
+                    minLength: {
+                      value: 4,
+                      message: "Minimum length should be 4",
+                    },
+                  })}
+                />
+                <FormErrorMessage>
+                  {errors.title && errors.title.message}
+                </FormErrorMessage>
+              </FormControl>
+              <FormControl
+                borderColor="lightGrayAlpha25"
+                isInvalid={errors.description}
+              >
+                <FormLabel htmlFor="description" variant="modal-subtitle">
+                  Description
+                </FormLabel>
+                <Textarea
+                  id="description"
+                  placeholder="It's always good to take a break. This  15 minute break will  recharge the batteries  a little."
+                  {...register("description", {
+                    required: "This is required",
+                    minLength: {
+                      value: 4,
+                      message: "Minimum length should be 4",
+                    },
+                  })}
+                />
+                <FormErrorMessage>
+                  {errors.description && errors.description.message}
+                </FormErrorMessage>
+              </FormControl>
+              <FormControl
+                borderColor="lightGrayAlpha25"
+                isInvalid={errors.subtask}
+              >
+                <FormLabel htmlFor="subtask" variant="modal-subtitle">
+                  Subtask
+                </FormLabel>
+                <Input
+                  id="subtask"
+                  placeholder="e.g Make coffee"
+                  {...register("subtask", {
+                    required: "This is required",
+                    minLength: {
+                      value: 4,
+                      message: "Minimum length should be 4",
+                    },
+                  })}
+                />
+                <FormErrorMessage>
+                  {errors.subtask && errors.subtask.message}
+                </FormErrorMessage>
+              </FormControl>
+              <Button
+                width="full"
+                variant="secondary"
+                isLoading={isSubmitting}
+                type="submit"
+              >
+                Add New Subtask
+              </Button>
+              <FormControl isInvalid={errors.subtask}>
+                <FormLabel htmlFor="subtask" variant="modal-subtitle">
+                  Subtask
+                </FormLabel>
+                <Select
+                  placeholder="Select option"
+                  iconColor="customPurple.500"
+                  borderColor="lightGrayAlpha25"
+                  fontSize="13px"
+                >
+                  <option value="option1">Option 1</option>
+                  <option value="option2">Option 2</option>
+                  <option value="option3">Option 3</option>
+                </Select>
+              </FormControl>
+              <Button
+                width="full"
+                variant="primary"
+                isLoading={isSubmitting}
+                type="submit"
+              >
+                Create Task
+              </Button>
+            </chakra.form>
+          </ModalBody>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+};
+
+export default Header;
