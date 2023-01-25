f2e46ed82add22a2c870f5d11ae0fa0a57a198cb
Jorge Eyzaguirre <jorgeyza@gmail.com>
2023-01-25 14:01:35 -0500

    Update Board component

--- src/components/Board/index.tsx
+++ src/components/Board/index.tsx
@@ -1,17 +1,39 @@
-import { Flex } from '@chakra-ui/react';
+import { Center, Flex, useColorModeValue } from "@chakra-ui/react";
 
-import Column from './Column';
+import Column from "./Column";
 
 const Board = () => {
+  const newColumnBackgroundColor = useColorModeValue("lightGray", "darkerGray");
+  const newColumnHoverBackgroundColor = useColorModeValue(
+    "purpleAlpha25",
+    "darkGray"
+  );
+
   return (
-    <Flex columnGap={6} paddingBottom={8} overflow='auto' data-test='board'>
+    <Flex
+      columnGap={6}
+      paddingBottom={8}
+      overflow="auto"
+      flexGrow={1}
+      data-test="board"
+    >
       <Column />
       <Column />
       <Column />
-      <Column />
-      <Column />
-      <Column />
-      <Column />
+      <Center
+        as="button"
+        minWidth="280px"
+        height="90%"
+        marginTop="34px"
+        borderRadius={8}
+        fontSize="24px"
+        fontWeight="bold"
+        color="customPurple.500"
+        backgroundColor={newColumnBackgroundColor}
+        _hover={{ backgroundColor: newColumnHoverBackgroundColor }}
+      >
+        + New Column
+      </Center>
     </Flex>
   );
 };
