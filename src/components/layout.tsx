9c817e8e2396bc79ff29bd1d47b218cdd4354426
Jorge Eyzaguirre <jorgeyza@gmail.com>
2023-01-25 14:02:00 -0500

    Update layout styles

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,15 +1,33 @@
-import { Flex } from '@chakra-ui/react';
+import { Flex } from "@chakra-ui/react";
 
-import Sidebar from '@/components/Sidebar';
-import Header from '@/components/Header';
+import Sidebar from "@/components/Sidebar";
+import Header from "@/components/Header";
 
-const Layout = ({ children, className }: { children: React.ReactNode; className: string }) => {
+const Layout = ({
+  children,
+  className,
+}: {
+  children: React.ReactNode;
+  className: string;
+}) => {
   return (
-    <Flex overflow='hidden' data-test='app-layout' className={className}>
+    <Flex overflow="hidden" data-test="app-layout" className={className}>
       <Sidebar />
-      <Flex flexDirection='column' width='full' data-test='right-side' overflow='auto'>
+      <Flex
+        flexDirection="column"
+        width="full"
+        overflow="auto"
+        data-test="right-side"
+      >
         <Header />
-        <Flex as='main' padding={6} flexGrow={1}>
+        <Flex
+          as="main"
+          flexGrow={1}
+          flexDirection="column"
+          alignItems="flex-start"
+          rowGap={4}
+          padding={6}
+        >
           {children}
         </Flex>
       </Flex>
