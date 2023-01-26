58b323ce5b9fda4ed365050f99977e753bd9891f
Jorge Eyzaguirre <jorgeyza@gmail.com>
2023-01-25 19:02:37 -0500

    Fix jotai Provider code position

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -26,7 +26,6 @@ const Layout = ({
           flexDirection="column"
           alignItems="flex-start"
           rowGap={4}
-          height="calc(100vh - 97px)"
           padding={6}
           data-test="main-container"
         >
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,24 +1,29 @@
-import { type AppType } from 'next/app';
-import { ChakraProvider } from '@chakra-ui/react';
-import { Plus_Jakarta_Sans } from '@next/font/google';
+import { type AppType } from "next/app";
+import { ChakraProvider } from "@chakra-ui/react";
+import { Plus_Jakarta_Sans } from "@next/font/google";
+import { atom, Provider } from "jotai";
 
-import Layout from '@/components/layout';
+import Layout from "@/components/layout";
 
-import { trpc } from '@/utils/trpc';
+import { trpc } from "@/utils/trpc";
 
-import theme from '@/theme';
+import theme from "@/theme";
 
 const plusJakartaSans = Plus_Jakarta_Sans({
-  weight: ['500', '700'],
-  subsets: ['latin']
+  weight: ["500", "700"],
+  subsets: ["latin"],
 });
+
+export const drawerAtom = atom(true);
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <ChakraProvider theme={theme}>
-      <Layout className={plusJakartaSans.className}>
-        <Component {...pageProps} />
-      </Layout>
+      <Provider>
+        <Layout className={plusJakartaSans.className}>
+          <Component {...pageProps} />
+        </Layout>
+      </Provider>
     </ChakraProvider>
   );
 };
