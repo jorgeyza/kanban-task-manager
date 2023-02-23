c15f1d6adae947a042960cb087fcda183c9a41ba
Jorge Eyzaguirre <jorgeyza@gmail.com>
2023-02-23 11:00:43 -0500

    Reduce code complexity

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -4,47 +4,66 @@ import seedData from "./seedData.json";
 const prisma = new PrismaClient();
 
 async function main() {
-  // Delete all existing records
-  await prisma.subtask.deleteMany();
-  await prisma.task.deleteMany();
-  await prisma.column.deleteMany();
-  await prisma.board.deleteMany();
+  await prisma.$transaction(async (prisma) => {
+    // Delete all existing records
+    await prisma.subtask.deleteMany();
+    await prisma.task.deleteMany();
+    await prisma.column.deleteMany();
+    await prisma.board.deleteMany();
 
-  // Create new records
-  for (let board of seedData.boards) {
-    const createdBoard = await prisma.board.create({
-      data: { title: board.title },
-    });
-
-    for (let column of board.columns) {
-      const createdColumn = await prisma.column.create({
+    // Create new records
+    for (let board of seedData.boards) {
+      const createdBoard = await prisma.board.create({
         data: {
-          title: column.title,
-          board: { connect: { id: createdBoard.id } },
+          title: board.title,
+          columns: {
+            create: board.columns.map((column) => ({
+              title: column.title,
+              tasks: {
+                create: column.tasks.map((task) => ({
+                  title: task.title,
+                  description: task.description,
+                  subtasks: {
+                    create: task.subtasks.map((subtask) => ({
+                      title: subtask.title,
+                      isDone: subtask.isDone,
+                    })),
+                  },
+                })),
+              },
+            })),
+          },
+        },
+        include: {
+          columns: {
+            include: {
+              tasks: {
+                include: {
+                  subtasks: true,
+                },
+              },
+            },
+          },
         },
       });
 
-      for (let task of column.tasks) {
-        const createdTask = await prisma.task.create({
-          data: {
-            title: task.title,
-            description: task.description,
-            column: { connect: { id: createdColumn.id } },
-          },
-        });
+      console.log(
+        `Created board ${createdBoard.title} with ${createdBoard.columns.length} columns`
+      );
 
-        for (let subtask of task.subtasks) {
-          await prisma.subtask.create({
-            data: {
-              title: subtask.title,
-              isDone: subtask.isDone,
-              task: { connect: { id: createdTask.id } },
-            },
-          });
+      for (let column of createdBoard.columns) {
+        console.log(
+          `Created column ${column.title} with ${column.tasks.length} tasks`
+        );
+
+        for (let task of column.tasks) {
+          console.log(
+            `Created task ${task.title} with ${task.subtasks.length} subtasks`
+          );
         }
       }
     }
-  }
+  });
 }
 
 main()
