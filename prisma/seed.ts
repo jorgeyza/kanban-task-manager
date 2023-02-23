0b3ba5ec8eea20c6ae3fddf27b51b826214074e7
Jorge Eyzaguirre <jorgeyza@gmail.com>
2023-02-23 16:16:29 -0500

    Update seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -4,66 +4,75 @@ import seedData from "./seedData.json";
 const prisma = new PrismaClient();
 
 async function main() {
-  await prisma.$transaction(async (prisma) => {
-    // Delete all existing records
-    await prisma.subtask.deleteMany();
-    await prisma.task.deleteMany();
-    await prisma.column.deleteMany();
-    await prisma.board.deleteMany();
+  try {
+    await prisma.$transaction(
+      async (tx) => {
+        // Delete all existing records
+        await tx.subtask.deleteMany();
+        await tx.task.deleteMany();
+        await tx.column.deleteMany();
+        await tx.board.deleteMany();
 
-    // Create new records
-    for (const board of seedData.boards) {
-      const createdBoard = await prisma.board.create({
-        data: {
-          title: board.title,
-          columns: {
-            create: board.columns.map((column) => ({
-              title: column.title,
-              tasks: {
-                create: column.tasks.map((task) => ({
-                  title: task.title,
-                  description: task.description,
-                  subtasks: {
-                    create: task.subtasks.map((subtask) => ({
-                      title: subtask.title,
-                      isDone: subtask.isDone,
+        // Create new records
+        for (const board of seedData.boards) {
+          const createdBoard = await tx.board.create({
+            data: {
+              title: board.title,
+              columns: {
+                create: board.columns.map((column) => ({
+                  title: column.title,
+                  tasks: {
+                    create: column.tasks.map((task) => ({
+                      title: task.title,
+                      description: task.description,
+                      subtasks: {
+                        create: task.subtasks.map((subtask) => ({
+                          title: subtask.title,
+                          isDone: subtask.isDone,
+                        })),
+                      },
                     })),
                   },
                 })),
               },
-            })),
-          },
-        },
-        include: {
-          columns: {
+            },
             include: {
-              tasks: {
+              columns: {
                 include: {
-                  subtasks: true,
+                  tasks: {
+                    include: {
+                      subtasks: true,
+                    },
+                  },
                 },
               },
             },
-          },
-        },
-      });
+          });
 
-      console.log(
-        `Created board ${createdBoard.title} with ${createdBoard.columns.length} columns`
-      );
-
-      for (const column of createdBoard.columns) {
-        console.log(
-          `Created column ${column.title} with ${column.tasks.length} tasks`
-        );
-
-        for (const task of column.tasks) {
           console.log(
-            `Created task ${task.title} with ${task.subtasks.length} subtasks`
+            `Created board ${createdBoard.title} with ${createdBoard.columns.length} columns`
           );
+
+          for (const column of createdBoard.columns) {
+            console.log(
+              `Created column ${column.title} with ${column.tasks.length} tasks`
+            );
+
+            for (const task of column.tasks) {
+              console.log(
+                `Created task ${task.title} with ${task.subtasks.length} subtasks`
+              );
+            }
+          }
         }
+      },
+      {
+        timeout: 30000,
       }
-    }
-  });
+    );
+  } catch (error) {
+    console.log("🚀 ~ file: seed.ts:69 ~ main ~ error:", error);
+  }
 }
 
 main()
