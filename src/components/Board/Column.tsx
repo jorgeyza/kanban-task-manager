160af3265e66f04ffc62f15b549f8766eb2911e7
Jorge Eyzaguirre <jorgeyza@gmail.com>
2023-02-25 00:14:32 -0500

    Add random color

--- src/components/Board/Column.tsx
+++ src/components/Board/Column.tsx
@@ -16,7 +16,12 @@ const Column = ({ id, title }: Props) => {
   return (
     <Flex direction="column" rowGap={5} w={280}>
       <Flex columnGap={3}>
-        <Box w="15px" h="15px" borderRadius="50%" bgColor="#49c4e5" />
+        <Box
+          w="15px"
+          h="15px"
+          borderRadius="50%"
+          bgColor={`#${Math.floor(Math.random() * 16777215).toString(16)}`}
+        />
         <Heading variant="board-column-title">{title}</Heading>
       </Flex>
       {allTasks?.map((task) => (
