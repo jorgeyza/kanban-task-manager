eb9f68ca37576c2449301ff4efb103790e064995
Jorge Eyzaguirre <jorgeyza@gmail.com>
2023-02-26 13:40:30 -0500

    Add create task procedure

--- src/server/api/routers/task.ts
+++ src/server/api/routers/task.ts
@@ -1,10 +1,25 @@
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 import {
-  deleteBoardSchema,
+  createTaskSchema,
   getAllTasksByColumnIdSchema,
 } from "~/schema/board.schema";
 
 export const taskRouter = createTRPCRouter({
+  create: publicProcedure.input(createTaskSchema).mutation(({ ctx, input }) => {
+    return ctx.prisma.task.create({
+      data: {
+        title: input.title,
+        description: input.description ?? "",
+        column: { connect: { id: input.columnId } },
+        subtasks: {
+          createMany: {
+            data: input.subtasks,
+          },
+        },
+      },
+    });
+  }),
+
   getAllByColumnId: publicProcedure
     .input(getAllTasksByColumnIdSchema)
     .query(({ ctx, input }) => {
