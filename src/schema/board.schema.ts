5d12e8eccfda55e6f8da8bd77c16e6de95bdf812
Jorge Eyzaguirre <jorgeyza@gmail.com>
2023-03-01 18:23:25 -0500

    Update board schema

--- src/schema/board.schema.ts
+++ src/schema/board.schema.ts
@@ -11,8 +11,9 @@ export const updateBoardSchema = z.object({
   id: z.string(),
   title: z.string().min(1),
   columns: z
-    .array(z.object({ id: z.string(), title: z.string() }))
+    .array(z.object({ id: z.string().optional(), title: z.string() }))
     .max(10, { message: "You can only have 10 columns per board" }),
+  columnsIdsToDelete: z.array(z.string()),
 });
 
 export const deleteBoardSchema = z.object({ id: z.string() });
