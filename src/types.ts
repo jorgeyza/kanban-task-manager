95ed51ad8251efc86966fd14bfe9d1f720475a6b
Jorge Eyzaguirre <jorgeyza@gmail.com>
2023-03-01 18:25:39 -0500

    Update types

--- src/types.ts
+++ src/types.ts
@@ -1,8 +1,12 @@
 import type { UseModalProps } from "@chakra-ui/react";
-import { type Subtask, type Task } from "@prisma/client";
 import { type DynamicChakraModalAction } from "./constants";
+import { type RouterOutputs } from "./utils/api";
 
 type ObjectValues<T> = T[keyof T];
+
+type Board = RouterOutputs["board"]["getOne"];
+type Task = RouterOutputs["task"]["getAllByColumnId"][0];
+type Subtask = RouterOutputs["subtask"]["getAllByTaskId"][0];
 
 export type HTMLProps = React.HTMLAttributes<HTMLElement>;
 export interface ChakraModalProps extends UseModalProps {
@@ -12,6 +16,7 @@ export interface DynamicChakraModalProps extends ChakraModalProps {
   action: ObjectValues<typeof DynamicChakraModalAction>;
   task?: Task;
   subtasks?: Subtask[];
+  board?: Board;
 }
 
 // export interface DynamicChakraModalProps<
