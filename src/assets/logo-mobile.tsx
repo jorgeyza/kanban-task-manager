089c9edb254dfe332bd435db2d17a3a87e9a8826
Jorge Eyzaguirre <jorgeyza@gmail.com>
2023-03-13 17:31:22 -0500

    Fix svg

--- src/assets/logo-mobile.tsx
+++ src/assets/logo-mobile.tsx
@@ -1,20 +1,30 @@
 const LogoMobile = () => {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="26" fill="none">
-      <g fill="#635FC7" clipPath="url(#a)">
-        <path d="M4 .5H2a2 2 0 0 0-2 2v21a2 2 0 0 0 2 2h2a2 2 0 0 0 2-2v-21a2 2 0 0 0-2-2Z" />
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 26" fill="none">
+      <g clipPath="url(#clip0_3_38)">
         <path
-          d="M13 .5h-2a2 2 0 0 0-2 2v21a2 2 0 0 0 2 2h2a2 2 0 0 0 2-2v-21a2 2 0 0 0-2-2Z"
-          opacity=".75"
+          d="M4 0.5H2C0.89543 0.5 0 1.39543 0 2.5V23.5C0 24.6046 0.89543 25.5 2 25.5H4C5.10457 25.5 6 24.6046 6 23.5V2.5C6 1.39543 5.10457 0.5 4 0.5Z"
+          fill="#635FC7"
         />
         <path
-          d="M22 .5h-2a2 2 0 0 0-2 2v21a2 2 0 0 0 2 2h2a2 2 0 0 0 2-2v-21a2 2 0 0 0-2-2Z"
-          opacity=".5"
+          opacity="0.75"
+          d="M13 0.5H11C9.89543 0.5 9 1.39543 9 2.5V23.5C9 24.6046 9.89543 25.5 11 25.5H13C14.1046 25.5 15 24.6046 15 23.5V2.5C15 1.39543 14.1046 0.5 13 0.5Z"
+          fill="#635FC7"
+        />
+        <path
+          opacity="0.5"
+          d="M22 0.5H20C18.8954 0.5 18 1.39543 18 2.5V23.5C18 24.6046 18.8954 25.5 20 25.5H22C23.1046 25.5 24 24.6046 24 23.5V2.5C24 1.39543 23.1046 0.5 22 0.5Z"
+          fill="#635FC7"
         />
       </g>
       <defs>
-        <clipPath id="a">
-          <path fill="#fff" d="M0 .5h24v25H0z" />
+        <clipPath id="clip0_3_38">
+          <rect
+            width="24"
+            height="25"
+            fill="white"
+            transform="translate(0 0.5)"
+          />
         </clipPath>
       </defs>
     </svg>
